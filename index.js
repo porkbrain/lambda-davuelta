2064c2285ac8debe1107343faa0a9eb8dba033e8
Michael Bausano <michael@joinpouch.com>
2019-02-12 14:01:41 +0000

    ok

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ exports.handler = async (event, _, callback) => {
       throw new Error('Number of matches too high.')
     }
 
-    const sns = await Promise.all(touples.map((touple) => {
+    const sns = await Promise.all(touples.map((Message) => {
       return new AWS.SNS({ apiVersion: '2010-03-31' })
         .publish({
-          Message: touple.join(' ; '),
+          Message,
           TopicArn: process.env.SNS_TOPIC
         })
         .promise()
@@ -31,13 +31,15 @@ exports.handler = async (event, _, callback) => {
 }
 
 const parseTouples = (body) => {
-  const regex = /(\d{12}).{0,10}([a-z0-9]{4,7}\/\d{2}\/\d{4})/gmi
+  body = body.replace(/\n/gmi, '')
 
-  const res = regex.exec(body.replace(/\n/gmi, ''))
+  const res = /(\d{12}).{0,10}([a-z0-9]{4,7}\/\d{2}\/\d{4})/is.exec(body)
 
-  if (!res || !res[1] || !res[2]) {
+  console.log('Parsed', body, 'via', res)
+
+  if (!res || !res[0]) {
     throw new Error('No touples found.')
   }
 
-  return [ res[1], res[2] ]
+  return res[0]
 }
