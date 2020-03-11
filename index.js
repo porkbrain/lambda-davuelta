eb07d3bd48b894e6eef9036de701d5ee48dbf4a6
bausano <michael@enhance.com>
2020-03-11 23:39:43 +0000

    Refactoring to use molcesko and to avoid using claudia js.

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 const AWS = require('aws-sdk')
 AWS.config.update({ region: 'eu-west-1' })
 
+/**
+ * Several different ways to parse the information from an email.
+ */
+const parsers = [
+  (body) => {
+    // Finds a number which is prepended by JIK string.
+    const jik = /JIK č\.(\d+),/.exec(body)
+    return jik ? [`molcesko: ${jik[0]}`] : []
+  },
+  (body) => {
+    // We replace all new lines which break the regex.
+    return String(body.replace(/\n/g, '_').toString('utf8'))
+      // Find the table cell with davuelta.
+      .split('DATEVUELTA')
+      .map((search) => {
+        // An id is a 12 digit number
+        const id = /\d{12}/gmi.exec(search)
+        // Reference starts with a sequence of a few characters and then with
+        // two numbers.
+        const reference = /[a-z0-9]{4,7}\/\d{2}\/\d{4}/gmi.exec(search)
+
+        return id && reference
+          ? `${id[0]} ; ${reference[0]}`
+          : null
+      })
+      .filter(Boolean)
+  }
+]
+
 exports.handler = async (event, _, callback) => {
   try {
-    const touples = parseTouples(event.body)
+    let tuples = []
+    for (const parser of parsers) {
+      tuples = parser(event.body)
+      if (tuples.length > 0) {
+        break
+      }
+    }
 
-    console.log('Touples', touples)
+    console.log('tuples', tuples)
 
-    if (touples.length > process.env.MAX_CODES) {
+    if (tuples.length > process.env.MAX_CODES) {
       throw new Error('Number of matches too high.')
     }
 
-    const sns = await Promise.all(touples.map((touple) => {
+    const sns = await Promise.all(tuples.map((touple) => {
       return new AWS.SNS({ apiVersion: '2010-03-31' })
         .publish({
           Message: touple,
@@ -30,7 +65,7 @@ exports.handler = async (event, _, callback) => {
   }
 }
 
-const parseTouples = (body) => {
+const parsetuples = (body) => {
   return String(body.replace(/\n/g, '_').toString('utf8'))
     .split('DATEVUELTA')
     .map((search) => {
